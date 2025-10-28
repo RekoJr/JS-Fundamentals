cf0ff1cf0904605f8837588598823105e9d580c0
RekoJr <83286610+RekoJr@users.noreply.github.com>
2025-10-28 14:08:59 +0300

    Update 1-multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,2 +1,7 @@
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "JavaScript is amazing";
 
-console.log('C is Fun','Javascript is amazing', 'Python is cool'); 
+console.log(line1);
+console.log(line2);
+console.log(line3);
